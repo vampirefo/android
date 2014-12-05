4a14fe87255b23df68062896b1b7df939ecad660
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2014-12-06 00:37:05 +0200

    updates: clean up commented out cherries
    Change-Id: I649859e483d79bb4fb3c6b441105096cee5473ce

--- updates.sh
+++ updates.sh
@@ -8,26 +8,8 @@ if [ "${android}" = "" ]; then
 	android=~/android/system
 fi
 
-# build: Handle boot images with custom makefile properly
-# cherries+=(76919)
-
-# build: Add option to disable block-based ota
-# cherries+=(78849)
-
-# Android: driver_nl80211: implement NoA/p2p_ps/set_wps_ie stubs
-# cherries+=(78120)
-
-# rild: Don't use the clientId argument with non-qcom hardware
-# cherries+=(78128)
-
-# capabilities ERANG error fix
-# cherries+=(78853)
-
 # Revert "Revert "Reenable support for non-PIE executables""
 cherries+=(79136)
-
-# libutils: refbase: jellybean mr1 compat
-# cherries+=(79142)
 
 # Implement UICC TVL Data decoding
 cherries+=(80227)
