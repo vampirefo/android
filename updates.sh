d4f7ca9214c3e22ad98a3713c368c15d2830e77e
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2014-12-09 09:03:19 -0800

    Revert "updates: Implement UICC TVL Data decoding"
    This reverts commit 8124308424512ae1c344fb93be20d92b7932c3b3.
    
    Conflicts:
    
    	updates.sh
    
    Merged
    
    Change-Id: Ib9718e27d84b9ce897abc35086260ebb2cf312b3

--- updates.sh
+++ updates.sh
@@ -11,7 +11,4 @@ fi
 # Revert "Revert "Reenable support for non-PIE executables""
 cherries+=(79136)
 
-# Implement UICC TVL Data decoding
-cherries+=(80227)
-
 ${android}/build/tools/repopick.py -b ${cherries[@]}
