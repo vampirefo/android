3e49b2b9a4077358557282f634eed1d53eeb12d7
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2015-04-02 02:40:12 +0300

    update paths
    Change-Id: Id7727dec1edbd414d69702f97c7bad050fa98053

--- updates.sh
+++ updates.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ -f ~/bin/paths-12.0.sh ]; then
-	source ~/bin/paths-12.0.sh
+if [ -f ~/bin/paths-12.1.sh ]; then
+	source ~/bin/paths-12.1.sh
 fi
 
 if [ "${android}" = "" ]; then
