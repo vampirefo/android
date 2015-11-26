a59f0df30e0dfe1bc938cc03f1d6a0a7eca1aadd
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2015-11-26 15:04:24 +0200

    update cherries for M
    Change-Id: I2cf4645eaa45e204c1ee3669642bb327502e9479

--- updates.sh
+++ updates.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-if [ -f ~/bin/paths-12.1.sh ]; then
-	source ~/bin/paths-12.1.sh
+if [ -f ~/bin/paths-13.0.sh ]; then
+	source ~/bin/paths-13.0.sh
 fi
 
 if [ "${android}" = "" ]; then
 	android=~/android/system
 fi
 
-# Fix GPS for old GPS HALs
-cherries+=(97902)
+# Implement UICC TLV Data decoding.
+cherries+=(114381)
+
+# Skip unknown tags instead of aborting when parsing UICC TLV data
+cherries+=(114382)
+
+# UiccController: add back registerForOn
+cherries+=(114805)
+
+# UiccController: use registerForAvailable only when persist.radio.apm_sim_not_pwdn is enabled
+cherries+=(114806)
 
 ${android}/build/tools/repopick.py -b ${cherries[@]}
